ce10d0e713da0c1f3ae6212f47da899e7661a9ec
Hassan Yusuf <hassanyousuf1999@gmail.com>
2022-12-21 21:19:53 -0500

    changed background color, added post message request useMutation

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,21 +1,87 @@
-import React from 'react'
-import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from '@apollo/client';
+import React, { useState } from 'react'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql, useMutation } from '@apollo/client';
 import Messages from './Messages';
 
 // Chards imp
-import { Container } from 'shards-react';
+import { Container, Row, Col, FormInput, Button } from 'shards-react';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
   cache: new InMemoryCache(),
 });
 
+// Mutation create Post
+const POST_MESSAGE = gql`
+    mutation($user: String!, $text: String!) {
+        postMessage(user: $user, text: $text)
+    }
+`;
 
 
 const Chat = () => {
+    const [state, setState] = useState({
+        user: "John",
+        text: ""
+    });
+
+    const [postMessage] = useMutation(POST_MESSAGE);
+
+    const onSend = () => {
+        if(state.text.length > 0) {
+            postMessage({
+                variables: state
+            });
+        }
+
+        setState({
+            ...state,
+            text: ''
+        });
+    }
+
   return (
     <Container>
-        <Messages user="Sam"/>
+        <Messages user={state.user}/>
+        <Row>
+            {/* User can change name */}
+            <Col xs={2} style={{padding: 0 }}>
+                <FormInput 
+                label="User"
+                placeholder="Name"
+                value={state.user}
+                onChange = {(e) => setState({
+                    ...state,
+                    user: e.target.value
+                })}
+                >
+                </FormInput>
+            </Col>
+            {/* User can enter text */}
+            <Col xs={8}>
+                <FormInput 
+                label="Text"
+                placeholder="Enter your text message"
+                value={state.text}
+                onChange = {(e) => setState({
+                    ...state,
+                    text: e.target.value
+                })
+            }
+            onKeyUp={(e) => {
+                if(e.keyCode === 13) {
+                    onSend();
+                }
+            }}
+                >
+                </FormInput>
+            </Col>
+            {/* User can send text */}
+            <Col xs={2}>
+                <Button 
+                onClick={() => onSend()}
+                >Send</Button>
+            </Col>
+        </Row>
     </Container>
   )
 }
